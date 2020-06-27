c82b2f62c6c1278189d470eaec0fa7442207e72b
ajaidanial <ajai@geopits.com>
2020-06-27 16:46:47 +0530

    added proptypes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,24 @@
 import { faCaretUp } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import PropTypes from 'prop-types'
 import React from 'react'
 
-export default function Footer() {
+export default function Footer({ footerText }) {
   return (
     <footer className="footer">
-      <small>&copy; All rights reserved. Developed by WLinkStudio.</small>
+      <small>{footerText}</small>
 
       <button>
         <FontAwesomeIcon icon={faCaretUp} />
       </button>
     </footer>
   )
+}
+
+Footer.propTypes = {
+  footerText: PropTypes.string
+}
+
+Footer.defaultProps = {
+  footerText: '© All rights reserved. Developed by WLinkStudio.'
 }
