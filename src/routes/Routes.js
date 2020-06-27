399bd57b3ea0ca6877a6efe60f3aeced401da9c6
ajaidanial <ajai@geopits.com>
2020-06-27 09:41:48 +0530

    init footer

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -23,33 +23,37 @@ import {
 } from 'constants/appConfig'
 import { Route, Switch } from 'react-router-dom'
 import React from 'react'
+import { Footer } from 'components'
 
 const Routes = () => {
   return (
-    <Switch>
-      <Route exact path={LANDING_PAGE_URL} component={LandingPage} />
-      <Route
-        exact
-        path={CART_DELIVERY_DETAILS_PAGE_URL}
-        component={CartDeliveryDetailsPage}
-      />
-      <Route
-        exact
-        path={CART_PRODUCT_DETAILS_PAGE_URL}
-        component={CartProductDetailsPage}
-      />
-      <Route exact path={CART_REVIEW_PAGE_URL} component={CartReviewPage} />
-      <Route
-        exact
-        path={PRODUCT_DETAIL_PAGE_URL}
-        component={ProductDetailPage}
-      />
-      <Route exact path={PRODUCT_LIST_PAGE_URL} component={ProductListPage} />
-      <Route exact path={ORDERS_PAGE_URL} component={OrdersPage} />
-      <Route exact path={WISHLIST_PAGE_URL} component={WishlistPage} />
-      <Route exact path={ADDRESSES_PAGE_URL} component={AddressesPage} />
-      <Route component={Page404} />
-    </Switch>
+    <>
+      <Switch>
+        <Route exact path={LANDING_PAGE_URL} component={LandingPage} />
+        <Route
+          exact
+          path={CART_DELIVERY_DETAILS_PAGE_URL}
+          component={CartDeliveryDetailsPage}
+        />
+        <Route
+          exact
+          path={CART_PRODUCT_DETAILS_PAGE_URL}
+          component={CartProductDetailsPage}
+        />
+        <Route exact path={CART_REVIEW_PAGE_URL} component={CartReviewPage} />
+        <Route
+          exact
+          path={PRODUCT_DETAIL_PAGE_URL}
+          component={ProductDetailPage}
+        />
+        <Route exact path={PRODUCT_LIST_PAGE_URL} component={ProductListPage} />
+        <Route exact path={ORDERS_PAGE_URL} component={OrdersPage} />
+        <Route exact path={WISHLIST_PAGE_URL} component={WishlistPage} />
+        <Route exact path={ADDRESSES_PAGE_URL} component={AddressesPage} />
+        <Route component={Page404} />
+      </Switch>
+      <Footer />
+    </>
   )
 }
 
