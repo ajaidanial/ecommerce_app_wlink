c4f57a8eec828385f91ea7cbad707bab4be04699
ajaidanial <ajai@geopits.com>
2020-06-29 08:05:05 +0530

    modal redux init

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { Modal } from 'react-bootstrap'
+import { toggleAuthModel } from 'appRedux/actions'
+import React, { Component } from 'react'
 
 export class AuthModal extends Component {
   render() {
+    const { showAuthModel, toggleAuthModel } = this.props
+
     return (
-      <Modal show={true} onHide={alert}>
+      <Modal show={showAuthModel} onHide={toggleAuthModel}>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
@@ -15,4 +19,13 @@ export class AuthModal extends Component {
   }
 }
 
-export default AuthModal
+const mapStateToProps = ({ app }) => {
+  const { showAuthModel } = app
+  return { showAuthModel }
+}
+
+const mapDispatchToProps = {
+  toggleAuthModel: toggleAuthModel
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AuthModal)
